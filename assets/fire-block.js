8418b2c3c9e75b28a99d678a585ba974074e953a
Zabir Anik <xaviranik.me@gmail.com>
2022-01-21 12:27:37 +0600

    add: gutenberg block script

---
+++ assets/fire-block.js
@@ -0,0 +1,31 @@
+wp.blocks.registerBlockType( 'wp-fire-gutenburg/fire-block', {
+    title: wp.i18n.__( 'WP Fire Gutenberg', 'wp-fire-gutenburg' ),
+    description: wp.i18n.__( 'Custom gutenburg blocks', 'wp-fire-gutenburg' ),
+    icon: 'smiley',
+    category: 'common',
+
+    edit: function () {
+        return wp.element.createElement(
+            'div',
+            { className: 'wp-fire-block' },
+            wp.element.createElement(
+                'h2',
+                null,
+                wp.i18n.__( 'WP Fire Gutenberg', 'wp-fire-gutenburg' )
+            ),
+            wp.element.createElement(
+                'p',
+                null,
+                wp.i18n.__( 'This is a custom Gutenberg block.', 'wp-fire-gutenburg' )
+            )
+        );
+    },
+
+    save: function () {
+        return wp.element.createElement(
+            'p',
+            { className: 'wp-fire-block' },
+            wp.i18n.__( 'Saved to database', 'wp-fire-gutenburg' )
+        );
+    }
+} );
